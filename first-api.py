ed68cae9ebcd3ac3f870995b3fe1d45034a6afd1
tdiallo <tdiallo@mansasn.com>
2022-11-25 18:35:51 +0000

    save work get_products by id

--- first-api.py
+++ first-api.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 
 app = FastAPI()
 
+products = [
+    {"id": 1, "name": "iPad", "price": 599},
+    {"id": 2, "name": "iPhone", "price": 999},
+    {"id": 3, "name": "iWatch", "price": 699},
+]
 
-@app.get("/home")
+
+@app.get("/products")
 def index():
-    return {"message": "Hello World"}
+    return products
+
+
+@app.get("/products/{id}")
+def get_products(id: int, response: Response):
+    for product in products:
+        if product.get('id') == id:
+            return product
+
+    response.status_code = 404
+    return "Product not found"

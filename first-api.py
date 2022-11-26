2574265caf470c5938fb4ca54f1a65d4bd71ecb0
tdiallo <tdiallo@mansasn.com>
2022-11-26 13:49:52 +0000

    end part2 tuto

--- first-api.py
+++ first-api.py
@@ -14,8 +14,17 @@ def index():
     return products
 
 
+@app.get("/products/search")
+def index(name, response: Response):
+    founded_products = [product for product in products if name.lower() in product.get('name').lower()]
+    if not founded_products:
+        response.status_code = 404
+        return "Product not found"
+    return founded_products if len(founded_products) > 1 else founded_products[0]
+
+
 @app.get("/products/{id}")
-def get_products(id: int, response: Response):
+def index(id: int, response: Response):
     for product in products:
         if product.get('id') == id:
             return product
